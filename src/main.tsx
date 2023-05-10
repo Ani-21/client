b7bcc5c4129a3c4a0c60deea8d42c5913b21b394
Ani-21 <kvikviniya.am@edu.spbstu.ru>
2023-05-10 18:03:09 +0300

    fix: base route

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/onConnect/">
       <CssBaseline />
       <ThemeProvider theme={theme}>
         <App />
